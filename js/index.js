c6826bdd13f2fffaec7d257ee076d0e7358c4251
JONATHAN ZHAO <jinqiangzhao2017@gmail.com>
2023-03-16 13:44:51 -0400

    bug fix

--- js/index.js
+++ js/index.js
@@ -18,25 +18,25 @@ function init(){
 	
 	const docBtns = document.querySelectorAll(".DocBtn");
 	docBtns.forEach((btn)=>{
-		const docFile = `md/${btn.id}-faq.md`;
-		window.location.search = `?doc=${docFile}`;
+		window.location.search = `?doc=${btn.id}`;
 	});
 }
 // load remote file to element docE
-function loadDoc(filename, docE) {
+function loadDoc(docName, docE) {
 
+	const docFile = `md/${docName}-faq.md`;
 	const xhttp = new XMLHttpRequest();
 	xhttp.onload = function() {
 		docE.innerHTML = marked.parse(this.responseText);
 	}
-	xhttp.open("GET", filename, true);
+	xhttp.open("GET", docFile, true);
 	xhttp.send();
 }
 
 
 window.onload = function() {
 	init();
-	var docName = "md/bash-faq.md"; // default doc
+	var docName ="bash"; // default doc
 	const docE = document.querySelector("#content");
 	const urlParams = new URLSearchParams(window.location.search);
 	if(urlParams.has('doc')){
